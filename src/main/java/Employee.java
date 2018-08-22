92d5a5a6d28e35e3a79c1ee064db906d83fb029a
gtt11 <39889180+gtt11@users.noreply.github.com>
2018-08-22 12:19:42 +1000

    Adds constructor, fields and getters for employee details

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,42 @@
 public class Employee {
+
+    private String firstName;
+    private String surname;
+    private String salary;
+    private String superRate;
+    private String paymentStartDate;
+    private String paymentEndDate;
+
+    public Employee(String firstName, String surname, String salary, String superRate, String paymentStartDate, String paymentEndDate) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.salary = salary;
+        this.superRate = superRate;
+        this.paymentStartDate = paymentStartDate;
+        this.paymentEndDate = paymentEndDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Integer getSalary() {
+        return Integer.valueOf(salary);
+    }
+
+    public Integer getSuperRate() {
+        return Integer.valueOf(superRate);
+    }
+
+    public String getPaymentStartDate() {
+        return paymentStartDate;
+    }
+
+    public String getPaymentEndDate() {
+        return paymentEndDate;
+    }
 }
