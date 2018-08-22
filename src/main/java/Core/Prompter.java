bc5ec9de7978a2e562cc6adfa7434b30959c5c70
gtt11 <39889180+gtt11@users.noreply.github.com>
2018-08-22 16:51:10 +1000

    Refactor prompter to minimise state mutation

--- src/main/java/Core/Prompter.java
+++ src/main/java/Core/Prompter.java
@@ -4,8 +4,6 @@ public class Prompter {
 
     private EmployeeDetailGetter employeeDetailGetter;
     private PayslipGenerator payslipGenerator;
-    private Employee employee;
-    private PaySlip payslip;
     private PayslipPresenter payslipOutputGenerator;
 
     public Prompter(EmployeeDetailGetter input, PayslipPresenter output, PayslipGenerator payslipGenerator) {
@@ -15,26 +13,26 @@ public class Prompter {
     }
 
     public void runApplication() {
-        getEmployee();
-        preparePayslip();
-        outputPayslip();
+        Employee employee = getEmployee();
+        PaySlip payslip = preparePayslip(employee);
+        outputPayslip(payslip);
     }
 
-    private void getEmployee() {
+    private Employee getEmployee() {
         String firstName = employeeDetailGetter.getFirstName();
         String surname = employeeDetailGetter.getSurname();
         String salary = employeeDetailGetter.getSalary();
         String superRate = employeeDetailGetter.getSuperRate();
         String paymentStartDate = employeeDetailGetter.getPaymentStartDate();
         String paymentEndDate = employeeDetailGetter.getPaymentEndDate();
-        this.employee = new Employee(firstName, surname, salary, superRate, paymentStartDate, paymentEndDate);
+        return new Employee(firstName, surname, salary, superRate, paymentStartDate, paymentEndDate);
     }
 
-    private void preparePayslip() {
-        payslip = payslipGenerator.getPayslip(employee);
+    private PaySlip preparePayslip(Employee employee) {
+        return payslipGenerator.getPayslip(employee);
     }
 
-    private void outputPayslip() {
+    private void outputPayslip(PaySlip payslip) {
         payslipOutputGenerator.outputPayslip(payslip);
     }
 
