565394cf207f862460c0f45e14f0b59234b2746f
gtt11 <39889180+gtt11@users.noreply.github.com>
2018-08-23 17:18:48 +1000

    Refactor employee to save string data as float for salary and super rate

--- src/main/java/Core/Employee.java
+++ src/main/java/Core/Employee.java
@@ -4,16 +4,16 @@ public class Employee {
 
     private String firstName;
     private String surname;
-    private String salary;
-    private String superRate;
+    private Float salary;
+    private Float superRate;
     private String paymentStartDate;
     private String paymentEndDate;
 
     public Employee(String firstName, String surname, String salary, String superRate, String paymentStartDate, String paymentEndDate) {
         this.firstName = firstName;
         this.surname = surname;
-        this.salary = salary;
-        this.superRate = superRate;
+        this.salary = Float.parseFloat(salary);
+        this.superRate = Float.parseFloat(superRate);
         this.paymentStartDate = paymentStartDate;
         this.paymentEndDate = paymentEndDate;
     }
@@ -23,11 +23,11 @@ public class Employee {
     }
 
     public Float getSalary() {
-        return Float.parseFloat(salary);
+        return salary;
     }
 
     public Float getSuperRate() {
-        return Float.parseFloat(superRate);
+        return superRate;
     }
 
     public String getPaymentStartDate() {
