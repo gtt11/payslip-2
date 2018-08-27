b8d446b2a24ce3aa4efd32058180cf3a6d00ba09
gtt11 <39889180+gtt11@users.noreply.github.com>
2018-08-27 13:18:06 +1000

    Saves Employee with first and last name from Console input

--- src/main/java/Console/ConsoleReader.java
+++ src/main/java/Console/ConsoleReader.java
@@ -1,4 +1,30 @@
 package Console;
 
-public class ConsoleReader {
+import Core.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ConsoleReader implements EmployeeDetailGetter {
+
+    private Scanner in;
+
+    public ConsoleReader(InputStream input) {
+        this.in = new Scanner(input);
+    }
+
+    public Employee getEmployee() {
+        String firstName = requestFirstName();
+        String surname = requestSurname();
+        return new Employee(firstName, surname, "65000", "9", "1 March", "30 March");
+    }
+
+    private String requestFirstName() {
+        return in.nextLine();
+    }
+
+    private String requestSurname() {
+        return in.nextLine();
+    }
+
+
 }
--- src/test/java/ConsoleReaderTest.java
+++ src/test/java/ConsoleReaderTest.java
@@ -1,4 +1,25 @@
+import java.io.*;
+import Console.ConsoleReader;
+import Core.Employee;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class ConsoleReaderTest {
 
+    private ConsoleReader consoleReader;
+
+    @Test
+    public void createsEmployee_withCorrectName() throws UnsupportedEncodingException {
+        createConsoleReader_withTestInput("John\nDoe");
+        Employee employee = consoleReader.getEmployee();
+        assertThat(employee.getFullName(), is("John Doe"));
+    }
+
+    public void createConsoleReader_withTestInput(String testUserInput) throws UnsupportedEncodingException {
+        InputStream inputStream = new ByteArrayInputStream(testUserInput.getBytes("UTF-8"));
+        consoleReader = new ConsoleReader(inputStream);
+    }
 
 }
