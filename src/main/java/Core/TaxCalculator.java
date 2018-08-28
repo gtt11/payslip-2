876f80db3f616484cb9361900aef4afb0eafd2e5
gtt11 <39889180+gtt11@users.noreply.github.com>
2018-08-28 12:46:55 +1000

    Add TaxCalculator interface with MonthlyTaxCalculator implementation

--- src/main/java/Core/TaxCalculator.java
+++ src/main/java/Core/TaxCalculator.java
@@ -1,38 +1,9 @@
 package Core;
 
-import DataStore.TaxBracketLoader;
-
 import java.io.FileNotFoundException;
 
+public interface TaxCalculator {
 
-public class TaxCalculator {
-
-    private TaxBracketLoader taxBracketLoader;
-
-    public TaxCalculator(TaxBracketLoader taxTaxBracketLoader) {
-        this.taxBracketLoader = taxTaxBracketLoader;
-    }
-
-    public int calculateMonthlyIncomeTax(float annualSalary) throws FileNotFoundException {
-        TaxBracket[] taxBrackets = taxBracketLoader.getSortedTaxBrackets();
-        for (int i = 0; i < taxBrackets.length; i++) {
-            if (annualSalary >= taxBrackets[i].getBracketMin()) {
-                return calculateMonthlyIncomeTax(taxBrackets[i], annualSalary);
-            }
-        }
-        return 0;
-    }
-
-    private int calculateMonthlyIncomeTax(TaxBracket taxBracket, float grossIncome) {
-        float totalIncomeTax = taxBracket.getBaseTax() + getMarginalTax(taxBracket, grossIncome);
-        float monthlyIncomeTax = totalIncomeTax / 12;
-        return Math.round(monthlyIncomeTax);
-    }
-
-    private float getMarginalTax(TaxBracket taxBracket, float grossIncome) {
-        float taxableIncome = grossIncome - taxBracket.getBracketMin() + 1;
-        float marginalTaxRateFraction = taxBracket.getMarginalTaxRate() / 100;
-        return taxableIncome * marginalTaxRateFraction;
-    }
+    int calculateIncomeTax(float grossIncome) throws FileNotFoundException;
 
 }
